b5c2dbb4fc324f8cc157bf6053f1e243deed93fe
prologic <prologic@localhost>
2006-11-20 08:43:31 +1000

    setup.py: fixed  * renamed bin scripts to kdb-xxx
    committer: James Mills <prologic@daisy.(none)>

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from distutils.core import setup
 
-import libkdb
+import kdb
 
 setup(name = "kdb",
-		version = libkdb.__version__,
-		description = libkdb.__desc__,
-		author = libkdb.__author__,
-		author_email = libkdb.__email__,
-		url = libkdb.__url__,
-		packages=["libkdb"],
-		scripts=["kdb"])
+		version = kdb.__version__,
+		description = kdb.__desc__,
+		author = kdb.__author__,
+		author_email = kdb.__email__,
+		url = kdb.__url__,
+		packages=["kdb", "kdb/plugins"],
+		scripts=["bin/kdb", "bin/kdb-notify", "bin/kdb-git-update"])
