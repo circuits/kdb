38a32c52e7048e32ec1c45773d5d3b653ab7384d
prologic <prologic@localhost>
2007-08-25 22:53:20 +1000

    More generic setup/build
    committer: James Mills <prologic@shortcircuit.net.au>

--- setup.py
+++ setup.py
@@ -1,19 +1,57 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
+import re
+
+name = os.path.basename(os.getcwd())
+pkg = __import__(name)
 
 try:
-	from setuptools import setup
+	import ez_setup
+	ez_setup.use_setuptools()
+	print "Using ez_setup"
 except ImportError:
+	pass
+
+try:
+	from setuptools import setup, find_packages
+	print "Using setuptools"
+except ImportError:
+	print "Using distutils"
 	from distutils.core import setup
 
-import kdb
+# borrowed from pymills.utils
+def getFiles(paths, tests=[os.path.isfile], pattern=".*", \
+		include_path=True):
+	"""getFiles(path, tests=[os.path.isfile], pattern=".*", \
+			include_path=True) -> list of files
 
-pkg = kdb
-__version__ = pkg.__version__
+	Return a list of files in the specified path
+	applying the predicates listed in tests returning
+	only the files that match the pattern.
+	"""
 
-setup(
-		name=pkg.__name__,
+	def testFile(file):
+		for test in tests:
+			if not test(file):
+				return False
+		return True
+
+	list = []
+	for path in paths:
+		files = os.listdir(path)
+		for file in files:
+			if testFile(os.path.join(path, file)) and \
+					re.match(pattern, file):
+				if include_path:
+					list.append(os.path.join(path, file))
+				else:
+					list.append(file)
+	return list
+
+def main():
+	setup(
+		name=name,
 		version=pkg.__version__,
 		description=pkg.__description__,
 		long_description=pkg.__doc__,
@@ -26,20 +64,15 @@ setup(
 		classifiers=pkg.__classifiers__,
 		license=pkg.__license__,
 		keywords=pkg.__keywords__,
-		platform=pkg.__platform__,
+		platforms=pkg.__platforms__,
+		packages=find_packages(),
+		scripts=getFiles(["scripts"]),
+		install_requires=pkg.__install_requires__,
+		setup_requires=pkg.__setup_requires__,
+		extras_require=pkg.__extras_require__,
+		entry_points=pkg.__entry_points__,
+		package_data=pkg.__package_data__,
+	)
 
-		packages=(
-			"kdb",
-			"kdb/plugins"
-			),
-
-		scripts=(
-			"bin/kdb",
-			"bin/kdb-notify",
-			"bin/kdb-git-update",
-			),
-
-		install_requires=(
-			"pymills"
-			)
-		)
+if __name__ == "__main__":
+	main()
