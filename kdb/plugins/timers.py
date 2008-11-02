a2ac2c41d63fda32886d3671cd099960c7380fa9
prologic <prologic@localhost>
2008-11-03 01:23:42 +1000

    plugins.timers: FIxed

--- kdb/plugins/timers.py
+++ kdb/plugins/timers.py
@@ -10,28 +10,30 @@ Provides a test command to create a message to be
 display in x seconds.
 """
 
-__ver__ = "0.0.1"
+__ver__ = "0.0.2"
 __author__ = "James Mills, prologic at shortcircuit dot net dot au"
 
-from pymills.event import listener
+from circuits import listener, Event, Timer
 
 from kdb.plugin import BasePlugin
+
+class Message(Event):
+	"""Message Event
+
+	args: target, message
+	"""
 
 class Timers(BasePlugin):
 	"Timers"
 
 	@listener("timer")
-	def onTIMER(self, name, length, channel, **data):
-		if data.has_key("target"):
-			if data.has_key("message"):
-				target = data["target"]
-				message = data["message"]
-				self.bot.ircPRIVMSG(target, message)
+	def onTIMER(self, target, message):
+		self.bot.ircPRIVMSG(target, message)
 
 	def cmdTIMER(self, source, length, message="Hello World"):
 		"""Create a new time with the given length and message
 		
-		Syntax: TIMER <length> [<message>]
+		Syntax: TIMER <length> [message]
 		"""
 
 		try:
@@ -39,6 +41,8 @@ class Timers(BasePlugin):
 		except ValueError:
 			return  "Invalid length specified"
 
-		self.env.timers.add("tesT", length,
-				target=source, message=message)
+		timer = Timer(length, Message(source, message))
+		self.manager += timer
+		self.env.timers.append(timer)
+
 		return "Okay timer set"
