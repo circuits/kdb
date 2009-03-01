4c80388bbfaba62dc5f2c98e75a29798310f46d1
prologic <prologic@localhost>
2009-03-01 21:40:19 +1000

    plugins.eval: Reenabled this plugins

--- kdb/plugins/eval.py
+++ kdb/plugins/eval.py
@@ -29,8 +29,6 @@ class Eval(BasePlugin):
         Syntax: EVAL <expr>
         """
 
-        return "Disabled"
-
         try:
             msg = str(eval(s)).split("\n")
         except Exception, e:
