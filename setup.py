a598ad1f203d605d7f5830e58cde3d3b2b8fcc4e
James Mills <James Mills@localhost>
2011-01-28 15:13:43 +1000

    Refactoring...

--- setup.py
+++ setup.py
@@ -1,29 +1,65 @@
 #!/usr/bin/env python
 
-from distutils.filelist import findall
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup, find_packages
+    HAS_SETUPTOOLS = True
+except ImportError:
+    from distutils.core import setup
+    HAS_SETUPTOOLS = False
 
-import kdb as pkg
+if not HAS_SETUPTOOLS:
+    import os
+    from distutils.util import convert_path
+
+    def find_packages(where=".", exclude=()):
+        """Borrowed directly from setuptools"""
+
+        out = []
+        stack = [(convert_path(where), "")]
+        while stack:
+            where, prefix = stack.pop(0)
+            for name in os.listdir(where):
+                fn = os.path.join(where, name)
+                if ("." not in name and os.path.isdir(fn) and 
+                        os.path.isfile(os.path.join(fn, "__init__.py"))):
+                    out.append(prefix+name)
+                    stack.append((fn, prefix + name + "."))
+
+        from fnmatch import fnmatchcase
+        for pat in list(exclude) + ["ez_setup"]:
+            out = [item for item in out if not fnmatchcase(item, pat)]
+
+        return out
 
 setup(
-		name=pkg.__name__,
-		version=pkg.__version__,
-		description=pkg.__description__,
-		long_description=pkg.__doc__,
-		author=pkg.__author__,
-		author_email=pkg.__author_email__,
-		maintainer=pkg.__maintainer__,
-		maintainer_email=pkg.__maintainer_email__,
-		url=pkg.__url__,
-		download_url=pkg.__download_url__,
-		classifiers=pkg.__classifiers__,
-		license=pkg.__license__,
-		keywords=pkg.__keywords__,
-		platforms=pkg.__platforms__,
-		packages=find_packages(),
-		install_requires=pkg.__install_requires__,
-		setup_requires=pkg.__setup_requires__,
-		extras_require=pkg.__extras_require__,
-		entry_points=pkg.__entry_points__,
-		package_data=pkg.__package_data__,
+    name="kdb",
+    description="Knowledge (IRC) Database Bot",
+    long_description=open("README", "r").read(),
+    author="James Mills",
+    author_email="James Mills, prologic at shortcircuit dot net dot au",
+    url="http://bitbucket.org/prologic/kdb/",
+    download_url="http://bitbucket.org/prologic/kdb/downloads/",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: No Input/Output (Daemon)",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Communications :: Chat :: Internet Relay Chat",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    license="MIT",
+    keywords="Knowledge Database IRC Bot Framework",
+    platforms="POSIX",
+    packages=find_packages("."),
+    entry_points="""
+    [console_scripts]
+    kdb = kdb.main:main
+    """,
+    install_requires=("circuits",),
+    setup_requires=("hgtools",),
+    use_hg_version={"increment": "0.01"},
 )
