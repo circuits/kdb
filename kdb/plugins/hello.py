3ccb8aa1802e0e7dd93e59a1e2c6f84a1e94b400
James Mills <prologic@shortcircuit.net.au>
2014-04-12 14:08:10 +1000

    Fixed Hello Plugin

--- kdb/plugins/hello.py
+++ kdb/plugins/hello.py
@@ -2,6 +2,7 @@
 # Date:     8th April 2014
 # Author:   James Mills, prologic at shortcircuit dot net dot au
 
+
 """hello Plugin"""
 
 
@@ -12,21 +13,42 @@ __author__ = "James Mills, prologic at shortcircuit dot net dot au"
 from circuits import Component
 
 
-from kdb.plugin import BasePlugin
+from ..plugin import BasePlugin
 
 
 class Commands(Component):
 
     channel = "commands"
 
-    def hello(self, source, target, message):
+    def hello(self, source, target, args):
+        """Say Hello.
+
+        Syntax: HELLO [<message>]
+        """
+
+        if not args:
+            message = "World!"
+        else:
+            message = args
+
         return "Hello {0:s}".format(message or "World!")
+
+    def say(self, source, target, args):
+        """Say.
+
+        Syntax: SAY <message>
+        """
+
+        if not args:
+            return "No message specified."
+
+        return args
 
 
 class Hello(BasePlugin):
     """Hello Plugin"""
 
     def init(self, *args, **kwargs):
-        super(Test, self).init(*args, **kwargs)
+        super(Hello, self).init(*args, **kwargs)
 
         Commands().register(self)
