370962b3d23b671a5c7d3156682e0f4c67f5c96c
James Mills <prologic@shortcircuit.net.au>
2014-04-12 14:16:02 +1000

    Backed out changeset a1b180e7e851

---
+++ kdb/plugins/rmessage.py
@@ -0,0 +1,63 @@
+# Module:   rmessage
+# Date:     24th September 2007
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+"""RMessage
+
+This plugin listens for remote message events and
+sends a Message Event into the system and returning
+any replies generated.
+"""
+
+__version__ = "0.0.3"
+__author__ = "James Mills, prologic at shortcircuit dot net dot au"
+
+from collections import deque
+
+from circuits import handler
+from circuits.net.protocols.irc import PRIVMSG
+
+from kdb.plugin import BasePlugin
+
+class RMessage(BasePlugin):
+
+    """RMessage plugin
+
+    This doesn't have any user commands available.
+    This plugin listens for remote message events and
+    sends a Message Event into the system and returning
+    any replies generated.
+
+    Depends on: remote
+    """
+
+    def __init__(self, env):
+        super(RMessage, self).__init__(env)
+
+        self._rlog = deque([], 5)
+
+    def cmdRLOG(self, source, target):
+        """View Remote Log
+
+        Syntax: RLOG
+        """
+
+        return ["Last 5 remote messages:"] + list(self._rlog)
+
+    @handler("message", channel="remote")
+    def remote_message(self, source="anonymous", target=None, message=""):
+        self._rlog.append(message)
+
+        ourself = self.env.bot.auth["nick"]
+
+        if not (target is None or target == ourself):
+            message = "<%s> %s" % (source, message)
+            self.fire(PRIVMSG(target, message))
+            return "Message to %s" % target
+        else:
+            if target is None:
+                target = ourself
+
+            r = self.fire(PRIVMSG(source, target, message))
+            reply = r or ""
+            return reply.strip()
