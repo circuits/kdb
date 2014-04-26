a1b4b733d8acbf15f727dfc47086bfd806674cb8
James Mills <prologic@shortcircuit.net.au>
2014-04-27 01:27:28 +1000

    Preparing 1.0.3 Development

--- kdb/version.py
+++ kdb/version.py
@@ -7,5 +7,5 @@
 So we only have to maintain version information in one place!
 """
 
-version_info = (1, 0, 2)  # (major, minor, patch, dev?)
+version_info = (1, 0, 3, "dev")  # (major, minor, patch, dev?)
 version = ".".join(map(str, version_info))
