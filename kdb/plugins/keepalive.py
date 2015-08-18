4353733d77babfa4d6c4daf0e5b9d10818d54365
James Mills <prologic@shortcircuit.net.au>
2015-08-18 21:29:15 +1000

    [plugins.keepalive]: Added LAG command (See #28)

--- kdb/plugins/keepalive.py
+++ kdb/plugins/keepalive.py
@@ -1,15 +1,43 @@
 from time import time
+from collections import deque
 
 
-from circuits import handler, Event, Timer
 from circuits.protocols.irc import request, Message
+from circuits import handler, Component, Event, Timer
+
+from funcy import rpartial, take
 
 
 from ..plugin import BasePlugin
 
 
+def avg(xs):
+    return sum(xs) / len(xs) if xs else 0
+
+
+def averages(xs):
+    return tuple(map(avg, map(rpartial(take, xs), range(5, 20, 5))))
+
+
 class keepalive(Event):
     """keepalive Event"""
+
+
+class Commands(Component):
+
+    channel = "commands"
+
+    def lag(self, source, target, args):
+        """Display lag (latency) averages
+
+        Syntax: LAG
+        """
+
+        lag = self.parent.data["lag"]
+
+        return "Lag: {0:d}ms {1:d}ms {2:d}ms ".format(
+            *averages(lag)
+        )
 
 
 class KeepAlive(BasePlugin):
@@ -28,10 +56,26 @@ class KeepAlive(BasePlugin):
     def init(self, *args, **kwargs):
         super(KeepAlive, self).init(*args, **kwargs)
 
+        self.interval = int(self.config.get("keepalive", {}).get("interval", 60))
+
+        # We want at most 15mins worth of data points
+        self.data.init(
+            {
+                "lag": deque([], (60 * 15) / self.interval)
+            }
+        )
+
         # Keep-Alive Timer
-        Timer(60, keepalive(), persist=True).register(self)
+        Timer(self.interval, keepalive(), persist=True).register(self)
+
+        Commands().register(self)
 
     @handler("keepalive")
     def keepalive(self):
-        timestamp = int(time() * 10000)
-        self.fire(request(Message("PING", "LAB{0}".format(timestamp))))
+        timestamp = int(time() * 1000)
+        self.fire(request(Message("PING", "LAG{0}".format(timestamp))))
+
+    @handler("pong")
+    def pong(self, source, target, timestamp):
+        latency = int(time() * 1000) - int(timestamp[3:])
+        self.data["lag"].append(latency)
